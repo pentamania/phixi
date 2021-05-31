0a830bfdf94e9a41e9be024d78b2ac485b2bc651
pentamania <tanigami7220@gmail.com>
2021-05-31 16:27:19 +0900

    [add&change] Add Sprite frame animation feature
    * Clone texture when setting texture (since texture.frame is updated individually)
    * Override phina FrameAnimation
    * Add setFrameIndex-method, srcRect-getter
    * Format

--- src/Sprite.ts
+++ src/Sprite.ts
@@ -1,41 +1,118 @@
-import { Sprite as PixiSprite, LoaderResource } from 'pixi.js';
+import {
+  Sprite as PixiSprite,
+  LoaderResource,
+  Rectangle,
+  Texture,
+} from 'pixi.js';
 import phina from 'phina.js';
 import { AssetType } from './types';
+const { FrameAnimation } = phina.accessory;
+const { AssetManager } = phina.asset;
 
-type TextureOrKey = PIXI.Texture | string;
+type PixiTextureOrKey = Texture | string;
+
+// Override phina FrameAnimation._updateFrame
+FrameAnimation.prototype._updateFrame = function (): void {
+  if (!this.currentAnimation) return;
+
+  var anim = this.currentAnimation;
+  if (this.currentFrameIndex >= anim.frames.length) {
+    if (anim.next) {
+      this.gotoAndPlay(anim.next);
+      return;
+    } else {
+      this.paused = true;
+      this.finished = true;
+      return;
+    }
+  }
+
+  var index = anim.frames[this.currentFrameIndex];
+  var frame = this.ss.getFrame(index);
+
+  // this.target.srcRect.set(frame.x, frame.y, frame.width, frame.height);
+  const sprite = this.target as Sprite;
+  sprite.texture.frame.x = frame.x;
+  sprite.texture.frame.y = frame.y;
+  sprite.texture.frame.width = frame.width;
+  sprite.texture.frame.height = frame.height;
+  sprite.texture.updateUvs();
+
+  // if (this.fit) {
+  //   this.target.width = frame.width;
+  //   this.target.height = frame.height;
+  // }
+};
 
 /**
- * helper
- * @param texture
+ * @param texture 
  */
-function _getTexture(texture: TextureOrKey) {
+function _cloneTexture(texture: PixiTextureOrKey): Texture {
   if (typeof texture === 'string') {
-    return Sprite.getTextureByKey(texture);
+    return Sprite.getTextureByKey(texture).clone()
   } else {
-    return texture;
+    return texture.clone();
   }
 }
 
 /**
- * PIXI.Sprite拡張クラス
+ * PIXI.Sprite+α class
  * 文字列keyでの指定ができるなど、phina.js Spriteクラスの特徴を追加
+ * textureはframe更新処理のため、cloneする
  */
 export class Sprite extends PixiSprite {
-  constructor(texture: TextureOrKey) {
-    super(_getTexture(texture));
+  _frameIndex: number = 0;
+
+  /**
+   * @param texture Texture will be cloned
+   * @returns
+   */
+  constructor(texture: PixiTextureOrKey) {
+    super(_cloneTexture(texture));
   }
 
-  setTexture(texture: TextureOrKey): this {
-    this.texture = _getTexture(texture);
+  setTexture(texture: PixiTextureOrKey): this {
+    this.texture = _cloneTexture(texture);
     return this;
   }
 
-  setFrameIndex() {
-    // TODO
+  /**
+   * @param index Index of frame. Number loops when it exceeds max index
+   * @param frameWidth
+   * @param frameHeight
+   * @returns
+   */
+  setFrameIndex(
+    index: number,
+    frameWidth: number = this._width,
+    frameHeight: number = this._height
+  ) {
+    const row = ~~(this.texture.baseTexture.width / frameWidth);
+    const col = ~~(this.texture.baseTexture.height / frameHeight);
+    const maxIndex = row * col;
+    index = index % maxIndex;
+
+    const x = index % row;
+    const y = ~~(index / row);
+    const srcRect = this.srcRect;
+    srcRect.x = x * frameWidth;
+    srcRect.y = y * frameHeight;
+    srcRect.width = frameWidth;
+    srcRect.height = frameHeight;
+
+    this.texture.updateUvs();
+
+    this._frameIndex = index;
+
+    return this;
+  }
+
+  get srcRect(): Rectangle {
+    return this.texture.frame;
   }
 
   static getTextureByKey(key: string) {
-    const resrc = (phina.asset.AssetManager.get(
+    const resrc = (AssetManager.get(
       AssetType.Pixi,
       key
     ) as unknown) as LoaderResource;
