a502f190bf58c10e99545c835b64b3d7bcc0c9c9
pentamania <tanigami7220@gmail.com>
2021-07-07 14:24:01 +0900

    [improve] Add error-handling for loadPixiAsset
    Fix typing, add comments too

--- src/helpers.ts
+++ src/helpers.ts
@@ -13,10 +13,23 @@ export function loadPixiAsset(
   key: string,
   path: string
 ): Promise<PIXI.LoaderResource> {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
+    // Since PIXI.Loader can't load while it is busy loading other asset,
+    // we have to create loader for each asset...
+    // TODO: Cache loader to reuse?
     const pixiLoader = new Loader();
+
     pixiLoader.add(key, path).load((_loader, resources) => {
-      resolve(resources[key]);
+      const resrc = resources[key];
+      if (!resrc) {
+        reject(`Loader error: Cannot find key:"${key}" in resources`);
+      } else {
+        resolve(resrc);
+      }
+    });
+
+    pixiLoader.onError.add(reason => {
+      reject(reason);
     });
   });
 }
