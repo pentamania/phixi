08c98b2689ec912f674af61d90e21b5bca100499
pentamania <tanigami7220@gmail.com>
2023-05-09 11:56:47 +0900

    [refactor] Rearrange Sprite code, update docs

--- src/Sprite.ts
+++ src/Sprite.ts
@@ -6,78 +6,25 @@ import {
 } from 'pixi.js';
 import phina from 'phina.js';
 import { AssetType } from './types';
+
 const { FrameAnimation } = phina.accessory;
 const { AssetManager } = phina.asset;
 type PhinaSprite = phina.display.Sprite;
 
 type PixiTextureOrKey = PixiTexture | string;
 
-// Override phina FrameAnimation._updateFrame
-FrameAnimation.prototype._updateFrame = function (): void {
-  if (!this.currentAnimation) return;
-
-  var anim = this.currentAnimation;
-  if (this.currentFrameIndex >= anim.frames.length) {
-    if (anim.next) {
-      this.gotoAndPlay(anim.next);
-      return;
-    } else {
-      this.paused = true;
-      this.finished = true;
-      return;
-    }
-  }
-
-  var index = anim.frames[this.currentFrameIndex];
-  var frame = this.ss.getFrame(index);
-
-  let sprite: PhinaSprite | PixiSprite = this.target;
-  if (
-    (sprite as PixiSprite).texture &&
-    (sprite as PixiSprite).texture instanceof PixiTexture
-  ) {
-    sprite = sprite as PixiSprite;
-    sprite.texture.frame.x = frame.x;
-    sprite.texture.frame.y = frame.y;
-    sprite.texture.frame.width = frame.width;
-    sprite.texture.frame.height = frame.height;
-    sprite.texture.updateUvs();
-  } else {
-    // Original phina Sprite
-    // PhinaSprite should have srcRect, but is missed in phina.js.d.ts...
-    (sprite as any).srcRect.set(frame.x, frame.y, frame.width, frame.height);
-  }
-
-  // Disable fitting
-  // Sprite width/height always matches with the frame in pixi.js
-  if (!(this.target instanceof PixiSprite) && this.fit) {
-    this.target.width = frame.width;
-    this.target.height = frame.height;
-  }
-};
-
 /**
- * @param texture
- */
-function _cloneTexture(texture: PixiTextureOrKey): PixiTexture {
-  if (typeof texture === 'string') {
-    return Sprite.getTextureByKey(texture).clone();
-  } else {
-    return texture.clone();
-  }
-}
-
-/**
- * PIXI.Sprite+α class
- * 文字列keyでの指定ができるなど、phina.js Spriteクラスの特徴を追加
- * textureはframe更新処理のため、cloneする
+ * PIXI.Sprite with features from phina.Sprite added
+ *
+ * Added features
+ * - 文字列keyでの指定ができる
+ * - Frame animation feature
  */
 export class Sprite extends PixiSprite {
   _frameIndex: number = 0;
 
   /**
-   * @param texture Texture will be cloned
-   * @returns
+   * @param texture Texture will be cloned for frame animation
    */
   constructor(texture: PixiTextureOrKey) {
     super(_cloneTexture(texture));
@@ -92,7 +39,6 @@ export class Sprite extends PixiSprite {
    * @param index Index of frame. Number loops when it exceeds max index
    * @param frameWidth
    * @param frameHeight
-   * @returns
    */
   setFrameIndex(
     index: number,
@@ -140,5 +86,62 @@ export class Sprite extends PixiSprite {
     source: string | PixiTexture | HTMLCanvasElement | HTMLVideoElement
   ) {
     return PixiSprite.from(source);
+  }
+}
+
+// Override phina's FrameAnimation._updateFrame to support PixiSprite
+FrameAnimation.prototype._updateFrame = function (): // this: phina.accessory.FrameAnimation // Some
+void {
+  if (!this.currentAnimation) return;
+
+  const anim = this.currentAnimation;
+  if (this.currentFrameIndex >= anim.frames.length) {
+    if (anim.next) {
+      this.gotoAndPlay(anim.next);
+      return;
+    } else {
+      this.paused = true;
+      this.finished = true;
+      return;
+    }
+  }
+
+  const index = anim.frames[this.currentFrameIndex];
+  const frame = this.ss.getFrame(index);
+
+  let sprite = this.target as PhinaSprite | PixiSprite;
+  if (
+    (sprite as PixiSprite).texture &&
+    (sprite as PixiSprite).texture instanceof PixiTexture
+  ) {
+    // PIXI.Sprite and has texture
+    sprite = sprite as PixiSprite;
+    sprite.texture.frame.x = frame.x;
+    sprite.texture.frame.y = frame.y;
+    sprite.texture.frame.width = frame.width;
+    sprite.texture.frame.height = frame.height;
+    sprite.texture.updateUvs();
+  } else {
+    // Original phina Sprite
+    // PhinaSprite should have srcRect
+    (sprite as any).srcRect.set(frame.x, frame.y, frame.width, frame.height);
+  }
+
+  // Disable fitting
+  // Sprite width/height always matches with the frame in pixi.js
+  if (!(this.target instanceof PixiSprite) && this.fit) {
+    this.target.width = frame.width;
+    this.target.height = frame.height;
+  }
+};
+
+/**
+ * @param texture
+ */
+function _cloneTexture(texture: PixiTextureOrKey): PixiTexture {
+  if (typeof texture === 'string') {
+    return Sprite.getTextureByKey(texture).clone();
+  } else {
+    return texture.clone();
   }
 }
